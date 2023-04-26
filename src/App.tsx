097a2a00d5a75d0e7d07b19a99841c2c9eaf8725
UnicornDad8 <unicorndad8@gmail.com>
2023-04-26 14:53:01 -0400

    Showing a loader indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
+import "./main.css";
 
 interface UserProp {
   id: number;
@@ -9,19 +10,29 @@ interface UserProp {
 function App() {
   const [users, setUsers] = useState<UserProp[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
+        setLoading(true);
+        const controller = new AbortController();
         const res = await axios.get<UserProp[]>(
-          "https://jsonplaceholder.typicode.com/users"
+          "https://jsonplaceholder.typicode.com/users",
+          { signal: controller.signal }
         );
         //.then((res) => setUsers(res.data))
         //.catch((err) => setError(err.message));
 
         setUsers(res.data);
+        setLoading(false);
+
+        return () => controller.abort();
       } catch (err) {
+        if (err instanceof CanceledError) return;
+
         setError((err as AxiosError).message);
+        setLoading(false);
       }
     };
 
@@ -31,6 +42,7 @@ function App() {
   return (
     <div>
       {error && <p>{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
